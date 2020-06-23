5eb641152f42c62c6f30f390294c617f652a7f5d
Khushbu Raval <khushi29315@gmail.com>
2020-06-23 12:15:48 +0530

    route change

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import home from '../components/home'
 import Menu from '../components/menu'
+import data from '../components/data'
 
 Vue.use(VueRouter)
 
   const routes = [
     {
       path: '/',
+      redirect: 'home',
+    },
+    {
+      path: '/vue.js-vuex-ex',//for github demo
       redirect: 'home',
     },
     {
@@ -23,7 +28,7 @@ Vue.use(VueRouter)
     {
       path: '/about',
       name: 'data',
-      component: () => import('../components/data.vue')
+      component: data
     }
 ]
 
