d15c07fb302a2258433ee3185148e76f8d1bbc26
Khushbu Raval <khushi29315@gmail.com>
2020-06-23 12:55:03 +0530

    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="/home">CAFE<b style="color:var(--g-color)">MS</b></a>
+      <router-link class="navbar-brand" to="/home">CAFE<b style="color:var(--g-color)">MS</b></router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
